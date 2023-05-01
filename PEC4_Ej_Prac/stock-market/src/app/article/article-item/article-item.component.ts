bc31a9e129951b827c236510ce344dba05bc9b4f
Carlos <carloscolomer99@gmail.com>
2023-05-01 12:05:01 +0200

    Creamos el componente y actualizamos la vista principal

---
+++ PEC4_Ej_Prac/stock-market/src/app/article/article-item/article-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'article-item',
+  templateUrl: './article-item.component.html',
+  styleUrls: ['./article-item.component.css']
+})
+export class ArticleItemComponent {
+
+}
