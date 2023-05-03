2b52a0d1f2f14bfd26a1ad996f02d78e536e172a
Carlos <carloscolomer99@gmail.com>
2023-05-03 16:52:55 +0200

    mostramos array de articulos

--- PEC4_Ej_Prac/stock-market/src/app/article/article-item/article-item.component.ts
+++ PEC4_Ej_Prac/stock-market/src/app/article/article-item/article-item.component.ts
@@ -1,4 +1,4 @@
-import { Component  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IArticle } from './IArticle';
 
 @Component({
@@ -8,7 +8,7 @@ import { IArticle } from './IArticle';
 })
 
 
-export class Article implements IArticle{
+export class Article implements OnInit, IArticle {
 
   name: string = '';
   imageUrl: string = '';
@@ -16,29 +16,45 @@ export class Article implements IArticle{
   isOnSale: boolean = false;
   quantityInCart: number = 0;
 
-  // name: string;
-  // imageUrl: string;
-  // price: number;
-  // isOnSale: boolean;
-  // quantityInCart: number;
-  
-  // constructor({name = '', imageUrl = '', price = 0, isOnSale = false, quantityInCart = 0}){
-  //   this.name = name;
-  //   this.imageUrl = imageUrl;
-  //   this.price = price;
-  //   this.isOnSale = isOnSale;
-  //   this.quantityInCart = quantityInCart;
-  // }
+  articles: Article[] = new Array<Article>();
 
-  ngOnInit() {   
 
-    this.name = 'Pizza';
-    this.imageUrl = 'https://images.unsplash.com/photo-1613564834361-9436948817d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1343&q=80';
-    this.price = 23;
-    this.isOnSale = true;
-    this.quantityInCart = 0;
+
+  constructor() {
 
   }
+
+  ngOnInit() {
+
+    
+
+    let articulo: Article = new Article();
+
+    articulo.name = 'Pizza';
+    articulo.imageUrl = 'https://images.unsplash.com/photo-1613564834361-9436948817d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1343&q=80';
+    articulo.price = 23;
+    articulo.isOnSale = true;
+    articulo.quantityInCart = 0;
+
+    this.articles.push(articulo);
+
+    articulo = new Article();
+
+    articulo.name = 'Pan';
+    articulo.imageUrl = 'https://images.unsplash.com/photo-1559811814-e2c57b5e69df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80';
+    articulo.price = 23;
+    articulo.isOnSale = true;
+    articulo.quantityInCart = 0;
+
+    this.articles.push(articulo);
+
+
+  }
+
+
+
+
+
 
 
 
